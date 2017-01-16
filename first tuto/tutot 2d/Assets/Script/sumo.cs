3780fdc289082f235953f1650f6fc27c027636a4
Meyer Florian <meyerf93@gmail.com>
2017-01-16 19:09:48 +0100

    synchronisation with portable

---
+++ first tuto/tutot 2d/Assets/Script/sumo.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sumo : MonoBehaviour {
+
+    public float speedRun;
+    public float ForceJump;
+
+    public float groundRadisu = 0.3f;
+    public LayerMask groundMask;
+    public Transform groundTest;
+
+    private Rigidbody2D playerRigid;
+    private Animator anim;
+
+    private bool isLeft = true;
+    private bool jump = false;
+    private bool isGrounded = false;
+
+	// Use this for initialization
+	void Start () {
+        playerRigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!jump)
+        {
+            float jumpVal = Input.GetAxis("Jump");
+            if (jumpVal > 0)
+            {
+                Debug.Log("JUMP");
+                jump = true;
+                Vector2 jumpVec = Vector2.up * ForceJump;
+                playerRigid.AddForce(jumpVec,ForceMode2D.Impulse);
+                anim.SetTrigger("jump");
+            }
+            
+        }
+	}
+
+    // update each regular time
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundTest.position, groundRadisu,groundMask);
+
+        if (jump && isGrounded)
+        {
+            jump = false;
+            anim.SetTrigger("TouchGround");
+        }
+
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("SpeedRun", Mathf.Abs(move));
+        Vector2 speedV = new Vector2(move*speedRun,playerRigid.velocity.y);
+
+        playerRigid.velocity = speedV;
+        if(move > 0 && isLeft)
+            flip();
+        else if (move < 0 && !isLeft)
+            flip();
+    }
+
+    private void flip()
+    {
+        isLeft = !isLeft;
+        Vector3 scale = transform.lossyScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
---
+++ first tuto/ufo tuto/Assets/Script/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody2D rb2d;
+
+    private int Count;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Count = 0;
+        winText.text = "";
+        SetCountText();
+    }
+   void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movment = new Vector2(moveHorizontal,moveVertical);
+        rb2d.AddForce(movment*speed);
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            Count++;
+            other.gameObject.SetActive(false);
+            SetCountText();
+        }        
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + Count.ToString();
+        if(Count >= 12)
+        {
+            winText.text = "You win";
+        }
+    }
+}
