a0cb6ea218ce9daf64d53c1e7f3b1a7d2cc22420
FlorianMeyer-PC\FlorianMeyer <florian.meyer@edu.hefr.ch>
2017-04-20 22:30:13 +0200

    done visite page

---
+++ Assets/Scripts/toogle_visibility.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class toogle_visibility : MonoBehaviour {
+
+	public GameObject Visible;
+	public GameObject Invisible;
+
+	public Toggle Button;
+
+	public	void toogle_visible(){
+		if (Button.isOn == true) {
+			Invisible.SetActive (false);
+			Visible.SetActive (true);
+		}
+		else {
+			Visible.SetActive (false);
+			Invisible.SetActive (true);
+		}
+	}
+}
