fb653680ffab646d819711cbe406ab797d2cb7cb
FlorianMeyer-PC\FlorianMeyer <florian.meyer@edu.hefr.ch>
2017-04-24 22:05:59 +0200

    add car things

---
+++ Assets/Scripts/change_color.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class change_color : MonoBehaviour {
+	public Toggle Button;
+	public Text item;
+
+	public void change_text_color_color(){
+		if (Button.isOn == true) {
+			item.color = Color.white;
+		}
+		else {
+			item.color = new Color (1, 1, 1);
+		}
+	}
+}
