4fa0d5a3103fe39d4642ef08f8fbcc63fee70e20
FlorianMeyer-PC\FlorianMeyer <florian.meyer@edu.hefr.ch>
2017-04-30 21:59:46 +0200

    add base unity

---
+++ Assets/Scripts/Change_color_electric.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Change_color_electric : MonoBehaviour {
+
+    public Toggle Button_electric_activity;
+    public Toggle Button_electric_season;
+    public Toggle Button_electric_sun;
+    public Toggle Button_electric_storage;
+
+    public Text text_activity;
+    public Text text_season;
+    public Text text_sun;
+    public Text texct_storage;
+
+    private Color start_color;
+
+    private void Start()
+    {
+        start_color = text_activity.color;
+    }
+
+    public void change_activity_color()
+    {
+        if (Button_electric_activity.isOn == true)
+        {
+            text_activity.color = Color.white;
+        }
+        else
+        {
+            text_activity.color = start_color;
+        }
+    }
+
+    public void change_season_color()
+    {
+        if (Button_electric_season.isOn == true)
+        {
+            text_season.color = Color.white;
+        }
+        else
+        {
+            text_season.color = start_color;
+        }
+    }
+
+    public void change_sun_color()
+    {
+        if (Button_electric_sun.isOn == true)
+        {
+            text_sun.color = Color.white;
+        }
+        else
+        {
+            text_sun.color = start_color;
+        }
+    }
+
+    public void change_storage_color()
+    {
+        if (Button_electric_storage.isOn == true)
+        {
+            texct_storage.color = Color.white;
+        }
+        else
+        {
+            texct_storage.color = start_color;
+        }
+    }
+}
---
+++ Assets/Scripts/change_color_light.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class change_color_light : MonoBehaviour {
+	public Toggle Button_light_quality;
+    public Toggle Button_light_types;
+
+    public Text text_quality;
+    public Text text_types;
+
+    private Color start_color;
+
+    private void Start()
+    {
+        start_color = text_quality.color;
+    }
+
+    public void change_quallity_color(){
+		if (Button_light_quality.isOn == true) {
+			text_quality.color = Color.white;
+		}
+		else {
+            text_quality.color = start_color;
+		}
+	}
+
+    public void change_types_color()
+    {
+        if (Button_light_types.isOn == true)
+        {
+            text_types.color = Color.white;
+        }
+        else
+        {
+            text_types.color = start_color;
+        }
+    }
+}
