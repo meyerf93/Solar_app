465fe9dc28e6d1884c260d6ebc955fd9769815c5
FlorianMeyer-PC\FlorianMeyer <florian.meyer@edu.hefr.ch>
2017-05-03 08:20:20 +0200

    add modification of main scene

---
+++ Assets/Scripts/connection_tinkee.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using uPLibrary.Networking.M2Mqtt;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+public class connection_tinkee : MonoBehaviour
+{
+
+    //http://tdoc.info/blog/2014/11/10/mqtt_csharp.html
+    private MqttClient4Unity client;
+
+    public string brokerHostname = null;
+    public int brokerPort = 1883;
+    /*public string userName = null;
+    public string password = null;*/
+    public string topic = null;
+
+    private Queue msgq = new Queue();
+
+    string lastMessage = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (brokerHostname != null )//&& userName != null && password != null)
+        {
+            Connect();
+            client.Subscribe(topic);
+            print("client connected and subscribed");
+            client.Publish(topic, System.Text.Encoding.ASCII.GetBytes("23.0"));
+            print("client send data");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        client.Publish(topic, System.Text.Encoding.ASCII.GetBytes("nice click!"));
+        while (client.Count() > 0)
+        {
+            string s = client.Receive();
+            msgq.Enqueue(s);
+            Debug.Log("received :" + s);
+        }
+
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            client.Publish(topic, System.Text.Encoding.ASCII.GetBytes("nice click!"));
+        }
+    }
+
+    void OnGUI()
+    {
+        if (msgq != null && msgq.Count > 0)
+        {
+            if (lastMessage == null)
+            {
+                lastMessage = (string)msgq.Dequeue();
+            }
+            else
+            {
+                lastMessage = (string)msgq.Dequeue();
+                lastMessage = null;
+            }
+        }
+        GUILayout.Label(lastMessage);
+    }
+
+    public void Connect()
+    {
+        client = new MqttClient4Unity(brokerHostname, brokerPort, false, null);
+        string clientId = Guid.NewGuid().ToString();
+        client.Connect(clientId);//, userName, password);
+    }
+
+    public void Publish(string _topic, string msg)
+    {
+        client.Publish(
+            _topic, Encoding.UTF8.GetBytes(msg),
+            MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, false);
+    }
+}
+
+
