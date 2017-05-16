24fa2715a6e71dcf686c79cd8584d315c68f81ed
FlorianMeyer-PC\FlorianMeyer <florian.meyer@edu.hefr.ch>
2017-05-16 22:41:02 +0200

    add conection broker

---
+++ Assets/HG/iot/mqtt/interfaces/IBrokerConnection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HG.iot.mqtt
+{
+	public interface IBrokerConnection
+	{
+		IIBrokerConnection Notify { get; }
+		void AddTopic(ITopic topic);
+		void RemoveTopic(ITopic topic);
+		string ClientId { get; }
+		bool Connect (ConnectionOptions options = null);
+		bool Disconnect ();
+		bool Reconnect ();
+		bool IsConnected { get; }
+		string Subscribe(ITopic topic);
+		string Unsubscribe(ITopic topic);
+		string Send(
+			ITopic topic, 
+			string message, 
+			bool isRetained = false, 
+			QualityOfServiceEnum qualityOfService = QualityOfServiceEnum.BestEffort);
+	}
+}
