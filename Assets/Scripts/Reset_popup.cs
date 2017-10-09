ad406c46770357978719584ecbfcef3e1698e822
FlorianMeyer-PC\FlorianMeyer <meyerf93@mail.com>
2017-10-09 03:20:48 +0200

    some smal correction

---
+++ Assets/Scripts/Reset_popup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reset_popup : MonoBehaviour
+{
+
+    public Toggle[] pop_up_list;
+
+    // Use this for initialization
+    public void ResetPopup()
+    {
+        for (int i = 0;i < pop_up_list.Length; i++)
+        {
+            pop_up_list[i].isOn = false;
+        }
+    }
+}
